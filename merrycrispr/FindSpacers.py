78a25c908771891e987d8db29f2c362651edd143
milescsmith <miles-smith@omrf.org>
2019-04-22 22:32:28 -0500

    Added revised off-target scoring functions

--- merrycrispr/FindSpacers.py
+++ merrycrispr/FindSpacers.py
@@ -28,134 +28,6 @@ from azimuth import model_comparison
 from on_target_score_calculator import calc_score
 
 
-class FormattedResult(object):
-    """ Class to automate the formatting of the final protospacers
-    """
-    __slots__ = ('GeneID', 'ExonRank', 'GeneName', 'seq', 'GC', 'position', 'score', 'offtargetscore')
-
-    # noinspection PyPep8
-    def __init__(self, recorddict):
-        # ['description','position','score','spacer','offtargetscore']
-        # description = 100767553|Unknown|Man1a1|||||1838438|1838994 1838822 0.111881537716
-        # GENEID | TRANSCRIPTID | GENENAME | EXON RANK | CONSTITUTIVE EXON | 5' UTR END | 3' UTR STOP | EXON START | EXON END
-        # GENEID | GENENAME | EXON RANK | SEQ_START | SEQ_END
-        tempdescript = recorddict['description'].split('|')
-        # self.GeneID = tempdescript[0]
-        # self.ExonRank = tempdescript[3]  # Ensembl ExonIDs are nearly useless for sorting as they not in numerical order
-        # self.GeneName = tempdescript[2]
-        # self.seq = str(recorddict['spacer'])
-        # self.GC = GC(self.seq)
-        # self.position = recorddict['position']
-        # self.score = recorddict['score']
-        # self.offtargetscore = recorddict['offtargetscore']
-        self.GeneID = tempdescript[0]
-        self.GeneName = tempdescript[1]
-        self.seq = str(recorddict['spacer'])
-        self.GC = GC(self.seq)
-        self.position = recorddict['position']
-        self.score = recorddict['score']
-        self.offtargetscore = recorddict['offtargetscore']
-
-    def __repr__(self):
-        return 'GeneID: {self.GeneID}, GeneName: {self.GeneName}, ExonRank: {self.ExonRank}, Sequence: {self.seq}, ' \
-               'Position: {self.position}, GC%: {self.GC}, score: {self.score}, ' \
-               'off-target score: {self.offtargetscore}'.format(self=self)
-#
-#
-# class m_SeqRecord(SeqRecord):
-#     """An altered form of SeqRecord that reads in and parses a header that contains the 5' UTR end and 3' UTR start
-#     information and uses that to trim the sequence
-#
-#     From BioMart, you need to select the following Attributes in this order:
-#     Ensembl Gene ID
-#     Ensembl Transcript ID
-#     Exon sequences
-#     Associated Gene Name
-#     Exon Rank in Transcript
-#     Constitutive Exon
-#     5' UTR End
-#     3' UTR Start
-#     Exon Chr Start (bp)
-#     Exon Chr End (bp)
-#
-#     If you are using GFFextractor, everything should already be formatted correctly
-#     """
-#
-#     __slots__ = ('id', 'tempseq', 'GeneID', 'TranscriptID', 'GeneName', 'ExonRank', 'ConstExon',
-#                  'FPUTRend', 'TPUTRstart', 'exonStart', 'exonEnd')
-#
-#     def __init__(self, seq, id="<unknown id>", name="<unknown name>", description="<unknown description>"):
-#         # Essentially, all the information we need other than the sequence is passed as the 'id',
-#         # with each portion separated by a '|', in the order defined above
-#         self.id = id
-#         self.tempseq = seq
-#         temp = self.id.split('|')
-#         self.GeneID = temp[0]
-#         self.TranscriptID = temp[1]
-#         self.GeneName = temp[2]
-#         self.ExonRank = temp[3]
-#         if temp[4] == '':
-#             self.ConstExon = False
-#         else:
-#             self.ConstExon = True
-#         try:
-#             self.FPUTRend = int(temp[5])
-#         except:
-#             self.FPUTRend = ''
-#         try:
-#             self.TPUTRstart = int(temp[6])
-#         except:
-#             self.TPUTRstart = ''
-#         try:
-#             self.exonStart = int(temp[7])
-#         except:
-#             self.exonStart = ''
-#         try:
-#             self.exonEnd = int(temp[8])
-#         except:
-#             self.exonEnd = ''
-#
-#         # If there is a 5'UTR
-#         if self.FPUTRend != '':
-#             # Cut off an amount of the sequence that is equal to the length of the UTR
-#             sequence = str(self.tempseq)[(self.FPUTRend - self.exonStart):(self.exonEnd - self.exonStart)]
-#         # The same, but do it for a 3'UTR
-#         elif self.TPUTRstart != '':
-#             sequence = str(self.tempseq)[0:(self.TPUTRstart - self.exonStart)]
-#         # Otherwise, leave the sequence alone
-#         else:
-#             sequence = str(self.tempseq)
-#
-#         SeqRecord.__init__(self, Seq(sequence, IUPAC.unambiguous_dna), id, name, description, dbxrefs=None,
-#                            features=None, annotations=None, letter_annotations=None)
-#
-#
-# def m_FastaIterator(handle, alphabet=single_letter_alphabet):
-#     """ FastaIterator from the Bio.SeqIO.FastaIO module altered to use the m_SeqRecord class
-#       Generator function to iterate over Fasta records (as SeqRecord objects).
-#
-#       Arguments:
-#        - handle - input file
-#        - alphabet - optional alphabet
-#        - title2ids - A function that, when given the title of the FASTA
-#          file (without the beginning >), will return the id, name and
-#          description (in that order) for the record as a tuple of strings.
-#          If this is not given, then the entire title line will be used
-#          as the description, and the first word as the id and name.
-#          :param handle:
-#          :param alphabet:
-#       """
-#
-#     from Bio.SeqIO.FastaIO import SimpleFastaParser
-#     for title, sequence in SimpleFastaParser(handle):
-#         try:
-#             first_word = title.split(None, 1)[0]
-#         except IndexError:
-#             assert not title, repr(title)
-#             first_word = ""
-#         yield m_SeqRecord(Seq(sequence, alphabet), id=first_word, name=first_word, description=title)
-
-
 def main():
 
 
@@ -208,8 +80,7 @@ def build_library(input_sequences: str=None,
         print("Sorry, no spacers matching that criteria were found")
         return 0
     else:
-        print("Finished scoring spacers.  {} of {} spacers have an on-target score above the cutoff threshold of "
-              "{}.\nBeginning Bowtie alignment...".format(len(spacerlist), initialnumber, cutoff))
+        print(f"Finished scoring spacers. {len(spacerlist)} of {initialnumber} spacers have an on-target score above the cutoff threshold of {cutoff}.\nBeginning Bowtie alignment...")
 
     ###--- Off-target discovery ---###
 
@@ -294,6 +165,7 @@ def build_library(input_sequences: str=None,
 
     except StopIteration:
         print("No spacers were found that correspond to the parameters you set.")
+
 
 # TODO need to keep strandedness in mind when searching for Cas13 guides
 def find_guides(itemlist: pyfaidx.Fasta,
@@ -399,7 +271,7 @@ def on_target_scoring(ruleset: str,
         spacers['score'] = spacerscores
         scoring_progress.finish()
     elif ruleset == "Azimuth":
-        spacers['score'] = model_comparison.predict(spacers['spacer'].as_matrix())
+        spacers['score'] = model_comparison.predict(spacers['spacer'].values)
     spacers = spacers[spacer_df['score'] > cutoff]
     return spacers
 
@@ -416,191 +288,11 @@ def score_entry(scoring_entry: dict,
     if scoring_entry['score'] > cutoff:
         return scoring_entry
 
-def off_target_discovery(spacerlist: list,
-                         cpus: int = 0,
-                         refgenome: str = "",
-                         large_index_size: bool = False,
-                         reject: bool = False) -> str:
-
-    if cpus is 0:
-        cpus = cpu_count()
-
-    # The below would be great if it actually worked.  Unfortunately, Bowtie will not allow us to set right-most
-    # nucleotides as an absolute requirement, only left-most.  I think I tried doing something where I flipped
-    # everthing around so that we could do that, but something horrible happened.
-    '''
-    # add the 'NGG' to the end of the spacer so we can restrict off-target analysis to those only those sites that
-    # have a pam
-    with open('temp.fa', 'w') as tempfile:
-        for entry in spacerlist:
-            tempfile.writelines(">%s %s %s\n%s\n" % (entry['description'], entry['position'],
-                                                     entry['score'], (entry['spacer']+'NGG')))
-    '''
-    # TODO: this has to be changed to work with the longspacer above
-    with open('temp.fa', 'w') as tempfile:
-        for entry in spacerlist:
-            tempfile.writelines(f">{entry['description']} {entry['position']} {entry['score']}\n{entry['spacer']}\n"
-                                )
-
-    # delete lists we are not going to use in the future to save on some memory
 
 
-    # off-target identification with Bowtie
-    # Use Bowtie to find if this particular sequence has any potential off targets (i. e. two or fewer mismatches)
-    # At current, Bowtie is set to return everything that a particular spacer matches within the reference genome
-    # with two or fewer mismatches.
-    # TODO switch to Bowtie2 so we can account for gaps in mismatches
-    # The problem with Bowtie2 is that it outputs in the SAM format.  Biopython does not have a SAM parser, though
-    # there is another library, pysam, that will read it.  This, of course, complicates things.  Bowtie, on the
-    # otherhand, will output to a plaintext file we can read.
-    # TODO modify this setup to account for NAG PAMs or do we even need to? (i. e. are we already?)
-    program = 'bowtie'
-    cpus = "-p" + str(cpus)
-    # So, if you go to tinker with this, note that there can be NO spaces in any of the strings you pass.  Maybe if
-    # you pass a variable (like 'refgenome'). The '--suppress' below is a good example - that is one argument passed to
-    # Bowtie, "--suppress 3,5,6,7", but everything goes all to hell unless you pass those as two separate arguments.
-    if reject:
-        if large_index_size:
-            check_output([program, '-a', "--suppress", "3,5,6,7", cpus, '-m', reject, '--large-index',
-                          refgenome, '-f', 'temp.fa', 'offtargets.fa'])
-        else:
-            check_output([program, '-a', "--suppress", "3,5,6,7", cpus, '-m', reject, refgenome, '-f',
-                          'temp.fa', 'offtargets.fa'])
-    else:
-        if large_index_size:
-            check_output([program, '-a', "--suppress", "3,5,6,7", cpus, '--large-index',
-                          refgenome, '-f', 'temp.fa', 'offtargets.fa'])
-        else:
-            check_output([program, '-a', "--suppress", "3,5,6,7", cpus, refgenome, '-f',
-                          'temp.fa', 'offtargets.fa'])
-
-    print("Bowtie finished.  Begining offtarget analysis...")
-    bowtie_results_file = 'offtargets.fa'
-
-    return bowtie_results_file
 
 
-def scoreofftarget(mismatched_positions: list) -> float:
-    """ Calculate the likelihood a protospacer will cut at a particular off-target site
-    Equation from http://crispr.mit.edu/about
 
-    The mismatch scoring algorithm from the Zhang group has three terms:
-    1) the effect of a mismatch at a particular position
-    2) the product of the mismatch scores, weighted by the mean distance between each mismatch
-    3) a penalty for the number of mismatches
-    Score is from 0 to 1, with higher scores being good.
-    """
-
-    # experimentally determined weighting penality for mismatch at each position
-    M = [0, 0, 0.014, 0, 0, 0.395, 0.317, 0, 0.389, 0.079, 0.445, 0.508, 0.613, 0.851, 0.731, 0.828,
-         0.615, 0.804, 0.685, 0.583]
-    if len(mismatched_positions) == 1:  # if there is only one mismatch, we should ignore the second two terms.
-        score = 1 - M[mismatched_positions[0]]
-        return score
-    else:
-        nmm = len(mismatched_positions)
-        mean_distance = (max(mismatched_positions) - min(mismatched_positions)) * 1.0 / (nmm - 1)
-        term_2 = (1 / ((((19 - mean_distance) / 19) * 4) + 1))
-        term_3 = 1.0 / (nmm ** 2)
-        term_1 = 1
-        for n in mismatched_positions:
-            term_1 *= 1 - M[n - 20]
-        score = term_1 * term_2 * term_3
-        return score
-
-
-def sumofftargets(offtargetlist: list) -> float:
-
-    sum_score = sum((scoreofftarget(x) for x in offtargetlist))
-    return 1.0 / (1 + sum_score) * 100
-
-
-def off_target_scoring(otrf: str,
-                       nuclease: str = 'Cas9',
-                       offtargetcutoff: int = 0,
-                       spacerlist: list = None,
-                       reject: bool = False) -> list:
-
-    if nuclease is 'Cas9':
-        # TODO: adjust this to account for other PAMs
-        mmpos = '[0-9]{1,}'
-        mmpos_re = compile(mmpos)
-        # these are so we ignore the pam when looking for mismatches
-        reject_pos = [20, 21, 22]
-
-    total_lines = check_output(['wc', '-l', otrf])
-    print(f"Total alignments from Bowtie: {total_lines}")
-
-    oftcount = 1
-    with open(otrf) as offtargetsfile:  # parse all the bowtie results into something we can use
-        # Read in the first line and parse.
-        keys = ['readname', 'strand', 'position', 'mmpositions']
-        lines = (line.strip('\n').split('\t') for line in offtargetsfile)
-        listofspacers = {f'{spacer["description"]} {spacer["position"]} {str(spacer["score"])}': spacer
-                         for spacer in spacerlist}
-
-        bowtie_widgets = ['Processing off-targets. Examining: ', progressbar.Counter(), ' ', progressbar.Percentage(),
-                          ' ', progressbar.Bar(), progressbar.Timer()]
-        bowtie_progress = progressbar.ProgressBar(widgets=bowtie_widgets, maxval=total_lines).start()
-
-        for line in lines:
-            bowtie_progress.update(oftcount)  # update the position on the progressbar
-            oftcount += 1  # update the count used by the progressbar
-            entry = dict(zip(keys, line))
-            # process the mismatch positions
-            perfectmatch = 0  # variable that keeps track of how many mismatches are between the guide and whatever
-            # Bowtie found
-            pos = mmpos_re.findall(entry['mmpositions'])
-            mmlist = None
-            if len(pos) == 0:  # Bowtie returns a match for the spacer itself in the genome
-                # if we have two such matches, we should indicate that there is a perfect off-target
-                perfectmatch += 1
-            else:
-                # Make a list of where the mismatches are, as long as they are in the first 20 nucleotides.
-                # We don't care what is in the mismatch position, only WHERE they are.
-                temp = [int(w) for w in pos if int(w) not in reject_pos]
-                if temp:
-                    mmlist = temp
-            # find the entry in list of spacers to which it belongs
-            # add the list of mismatches to that entry
-            if perfectmatch > 1:
-                listofspacers[entry['readname']]['otpositions'].append('discard')
-            elif mmlist:
-                listofspacers[entry['readname']]['otpositions'].append(mmlist)
-        bowtie_progress.finish()
-
-        matching_widgets = ['Matching off-targets. Examining: ', progressbar.Counter(), ' ', progressbar.Percentage(),
-                            ' ', progressbar.Bar(), progressbar.Timer()]
-        matching_progress = progressbar.ProgressBar(widgets=matching_widgets, maxval=len(listofspacers)).start()
-        matching_pos = 0
-
-        for key in listofspacers:
-            matching_pos += 1
-            matching_progress.update(matching_pos)
-            discard = False
-            # Tally up the offtarget score for the set of off targets and set the spacer's off-target score
-            for x in listofspacers[key]['otpositions']:
-                if 'discard' in x:  # just skip over anything we found has more than one other perfect match.
-                    discard = True
-            if len(listofspacers[key]['otpositions']) != 0 or discard:
-                if discard:
-                    listofspacers[key]['offtargetscore'] = 0
-                # Speed this up by rejecting things with over a certain set of matching off-targets
-                elif reject and len(listofspacers[key]['otpositions']) > reject:
-                    listofspacers[key]['offtargetscore'] = 0
-                else:
-                    listofspacers[key]['offtargetscore'] = sumofftargets(listofspacers[key]['otpositions'])
-
-        matching_progress.finish()
-
-    print("\nFinished scoring off-targets.")
-
-    # There is no need to process spacers that have an off-target score below the cutoff value
-    # so we make a new list with which we can work that is populated with the good stuff
-    prunedlist = [listofspacers[spacer] for spacer in listofspacers if
-                  float(listofspacers[spacer]['offtargetscore']) >= float(offtargetcutoff)]
-
-    return prunedlist
 
 
 def assemble_guide_list(gene_name: str,
